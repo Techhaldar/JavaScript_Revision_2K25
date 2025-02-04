f8854079baa77c7cfbe9311e444a67873dc63324
Ratin <ratinhaldar123@gmail.com>
2025-02-04 10:23:33 +0530

    Function and Parameter Updated

--- 03_JS_Basics/01_Functions.js
+++ 03_JS_Basics/01_Functions.js
@@ -1,39 +1,39 @@
-// function Hello() {
-//     console.log("H");
-//     console.log("E");
-//     console.log("L");
-//     console.log("L");
-//     console.log("o");
-// }
+function Hello() {
+    console.log("H");
+    console.log("E");
+    console.log("L");
+    console.log("L");
+    console.log("o");
+}
 
-// Hello();
+Hello();
 
-// function sayMyName() {
-//     console.log("Ratin");
-// }
+function sayMyName() {
+    console.log("Ratin");
+}
 
-// sayMyName()
-// sayMyName()
-// sayMyName()
+sayMyName()
+sayMyName()
+sayMyName()
 
-//  function addTwoNumbers(number1, number2) {
-//      let sum = number1 - number2;
-//      return sum;
-//  console.log(sum)
-//  }
+function addTwoNumbers(number1, number2) {
+    let sum = number1 - number2;
+    return sum;
+    console.log(sum)
+}
 
-// function addNumber(number1, number2) {
-//     let sum = number1 + number2;
-//     return sum;
-// }
-// const result = addNumber(4, 6)
-// console.log("Result:", result);
+function addNumber(number1, number2) {
+    let sum = number1 + number2;
+    return sum;
+}
+const result = addNumber(4, 6)
+console.log("Result:", result);
 
 
-// const result = addTwoNumbers(4, 5);
+const result1 = addTwoNumbers(4, 5);
 
-// console.log("Result: ",result);
-//  addTwoNumbers(5, 4)
+console.log("Result: ", result1);
+addTwoNumbers(5, 4)
 
 function loginUserMessage(username) {
     if (!username) {
@@ -44,32 +44,32 @@ function loginUserMessage(username) {
 
 console.log(loginUserMessage())
 
-// function calculateCartPrice(val1, vla2, val3, ...num) {
-//     return num;
-// }
+function calculateCartPrice(val1, vla2, val3, ...num) {
+    return num;
+}
 
-// console.log(calculateCartPrice(200, 500, 300, 100, 900))
+console.log(calculateCartPrice(200, 500, 300, 100, 900))
 
-// const user = {
-//     username: "ratin",
-//     price: 199
-// }
+const user = {
+    username: "ratin",
+    price: 199
+}
 
-// function handlObject(anyobject){
-//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
-// }
+function handlObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
 
-// console.log(handlObject(user));
+console.log(handlObject(user));
 
-// handlObject({
-//     username: "ratin haldar",
-//     price: 599
-// })
+handlObject({
+    username: "ratin haldar",
+    price: 599
+})
 
-// const myNewArray = [200, 400, 600, 800]
-// function returnSecondValue(getArray) {
-//     return getArray[2]
-// }
+const myNewArray = [200, 400, 600, 800]
+function returnSecondValue(getArray) {
+    return getArray[2]
+}
 
-// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue(myNewArray))
 
