9ec4260c4e3bf36f41192977c3cfdcf0e3481bd9
Ratin <ratinhaldar123@gmail.com>
2025-02-04 11:26:08 +0530

    Functions with objects and array

--- 03_JS_Basics/01_Functions.js
+++ 03_JS_Basics/01_Functions.js
@@ -31,7 +31,6 @@ console.log("Result:", result);
 
 
 const result1 = addTwoNumbers(4, 5);
-
 console.log("Result: ", result1);
 addTwoNumbers(5, 4)
 
@@ -47,19 +46,39 @@ console.log(loginUserMessage())
 function calculateCartPrice(val1, vla2, val3, ...num) {
     return num;
 }
-
 console.log(calculateCartPrice(200, 500, 300, 100, 900))
+
+function calculateCartPrice(...num) {
+    return num;
+}
+const price = calculateCartPrice(4, 5, 6);
+let sum = 0;
+for (let i = 0; i < price.length; i++) {
+    sum += price[i]
+}
+
+console.log(sum);
 
 const user = {
     username: "ratin",
     price: 199
 }
-
 function handlObject(anyobject) {
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
 }
-
 console.log(handlObject(user));
+
+const user = {
+    name: "ratin",
+    email: "ratinhaldar123@gmail.com"
+}
+
+function userLogedIn(anyobject) {
+    console.log(`Name is ${anyobject.name} and email Id is ${anyobject.email}`)
+}
+
+console.log(userLogedIn(user));
+
 
 handlObject({
     username: "ratin haldar",
@@ -70,6 +89,4 @@ const myNewArray = [200, 400, 600, 800]
 function returnSecondValue(getArray) {
     return getArray[2]
 }
-
 console.log(returnSecondValue(myNewArray))
-
