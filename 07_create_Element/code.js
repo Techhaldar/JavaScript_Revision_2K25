26886f11af0bc68d7265395433cedc08d0ac4c83
Ratin <ratinhaldar123@gmail.com>
2025-02-16 10:19:57 +0530

    All DOM complete revision

--- 07_create_Element/code.js
+++ 07_create_Element/code.js
@@ -3,11 +3,29 @@ console.log(div)
 console.log(div.innerText = 'ratin haldar')
 div.style.backgroundColor = "green"
 div.style.color = "#fff"
-div.style.padding = '23px'
-div.style.fontSize = '70px'
+div.style.padding = '12px'
+div.style.fontSize = '30px'
 div.className = "main"
 div.id = Math.round(Math.random() * 10 + 1)
 
 div.setAttribute("name", "first_div")
 
 document.body.appendChild(div)
+
+const parent = document.querySelector('.parent')
+console.log(parent);
+
+console.log(parent.children);
+console.log(parent.children[1].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+  console.log(parent.children[i].innerHTML);
+}
+
+parent.children[1].style.color = 'orange'
+const anotherDiv = document.createElement('div')
+console.log(anotherDiv.innerHTML = "This is ratin")
+anotherDiv.style.backgroundColor = 'red'
+anotherDiv.style.padding = '23px'
+anotherDiv.style.marginTop = '12px'
+document.body.appendChild(anotherDiv)
--- 07_create_Element/create_New_DOM.js
+++ 07_create_Element/create_New_DOM.js
@@ -1,23 +1,19 @@
 const parent = document.querySelector('.parent')
-// console.log(parent)
-// console.log(parent.children)
-// console.log(parent.children[1].innerText)
-// for(let i = 0; i < parent.children.length; i++){
-//     console.log(parent.children [i].innerText)
-// }
-// for (const element of parent.children) {
-//     console.log(element.innerText)
-// }
-// console.log(parent.firstElementChild)
+console.log(parent)
+console.log(parent.children)
+console.log(parent.children[1].innerText)
+for (let i = 0; i < parent.children.length; i++) {
+  console.log(parent.children[i].innerText)
+}
+for (const element of parent.children) {
+  console.log(element.innerText)
+}
+console.log(parent.firstElementChild)
 
-// const dayOne = document.querySelector('.day')
-// console.log(dayOne);
-// const dayTwo = (dayOne.nextElementSibling)
+ const dayOne = document.querySelector('.day')
+ console.log(dayOne);
+ const dayTwo = (dayOne.nextElementSibling)
 
-// console.log(dayTwo.nextElementSibling)
+ console.log(dayTwo.nextElementSibling)
 
-// console.log("NODES:", parent.childNodes)
-
-
-
-
+ console.log("NODES:", parent.childNodes)
