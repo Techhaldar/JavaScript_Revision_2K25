95d4722c9ab4c95af327618bf343ca3a8875c06d
Ratin <ratinhaldar123@gmail.com>
2025-02-24 22:22:11 +0530

    Prototype in JavaScript

--- 16_Prototype/script.js
+++ 16_Prototype/script.js
@@ -1,26 +1,25 @@
 //************ new keyword *************
-// function fullName (){
-    //     console.log("hello")
-    // }
-    
-    // console.log(fullName())
-    // console.log(fullName.prototype);
-    
-    // function createUser (username, score){
-        //     this.username = username
-        //     this.score = score
-        // }
+function fullName() {
+    console.log("hello")
+}
+console.log(fullName())
+console.log(fullName.prototype);
+
+function createUser(username, score) {
+    this.username = username
+    this.score = score
+}
 
 //************ prototype *************
-// let myName = "ratin haldar    ";
-// console.log(myName.truelength)
+let myName = "ratin haldar    ";
+console.log(myName.truelength)
 
-// let myHeros = ["thor", "spiderman", "hulk"]
+let myHeros = ["thor", "spiderman", "hulk"]
 
-// let heroPower = {
-//     thor :"hammer",
-//     spiderman : "sling",
-//     getSpiderPower : function (){
-//         console.log(`Spidy power is ${this.spiderman}`)
-//     }
-// }
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+    getSpiderPower: function () {
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
